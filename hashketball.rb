e05e5abc2b324c0452caf1024c9d51d400ba9849
mcelhinneyk <mr.mcelhinney@gmail.com>
2022-01-16 19:10:56 -0800

    commit tree 033cea8ef02960ab291f35132bdef18895958137

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code below game_hash
 def game_hash
   {
@@ -126,4 +127,125 @@ def game_hash
   }
 end
 
-# Write code here
+def players
+  all_players_data = []
+  game_hash.each do |side,team|
+    team[:players].each {|player| all_players_data << player}
+  end
+  all_players_data
+end
+
+def player_names
+  players.map {|player| player[:player_name]}
+end
+
+def home_players
+  game_hash[:home][:players]
+end
+
+def away_players
+  game_hash[:away][:players]
+end
+
+def num_points_scored (player_looking_for)
+  player_stats(player_looking_for)[:points]
+end
+
+def shoe_size(player_looking_for)
+  player_stats(player_looking_for)[:shoe]
+end
+
+def team_colors(team_looking_for)
+  game_hash.each do |side, team|
+    if team[:team_name] == team_looking_for
+      return team[:colors]
+    end
+  end
+end
+
+def team_names
+  team_names_array = []
+  game_hash.each {|side, team| team_names_array << team[:team_name]}
+  team_names_array
+end
+
+def player_numbers (team_looking_for)
+  player_numbers_array = []
+  game_hash.each do |side, team|
+    if team_looking_for == team[:team_name]
+      if side == :home
+        home_players.each {|player| player_numbers_array << player[:number]}
+      else
+        away_players.each {|player| player_numbers_array << player[:number]}
+      end
+    end
+  end
+  player_numbers_array
+end
+
+def player_stats (player_looking_for)
+  players.each do |player|
+    if player[:player_name] == player_looking_for
+      return player
+    end
+  end
+end
+
+def big_shoe_rebounds
+  biggest_shoe = 0
+  player_name = ""
+  player_names.each do |player|
+    shoe = shoe_size(player)
+    if shoe > biggest_shoe
+      biggest_shoe = shoe
+      player_name = player
+    end
+  end
+  player_stats(player_name)[:rebounds]
+end
+
+def most_points
+  most_points = 0
+  player_name = ""
+  player_names.each do |player|
+    player_points = num_points_scored(player)
+    if player_points > most_points
+      most_points = player_points
+      player_name = player
+    end
+  end
+  player_name
+end
+
+def winning_team
+  scores = {}
+  home_name = game_hash[:home][:team_name]
+  away_name = game_hash[:away][:team_name]
+  home_score = home_players.map {|player| player[:points]}.sum
+  away_score = away_players.map {|player| player[:points]}.sum
+  scores[home_name] = home_score
+  scores[away_name] = away_score
+  scores.max_by {|k,v| v}[0]
+end
+
+def player_with_longest_name
+  length_array = {}
+  player_names.each do |player|
+    name = player
+    size = player.length
+    length_array[name] = size
+  end
+  length_array.max_by {|k,v| v}[0]
+  
+end
+
+def long_name_steals_a_ton?
+  steals = {}
+  players.each do |player|
+    name = player[:player_name]
+    num_of_steals = player[:steals]
+    steals[name] = num_of_steals
+  end
+  most_steals = steals.max_by {|k,v| v}[0]
+  most_steals == player_with_longest_name
+end
